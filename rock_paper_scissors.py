0f0fe113c3d080646fd517e47137c5a97b12c657
kevinw123 <kevinwong_895@hotmail.com>
2016-07-06 22:48:06 -0700

    Adding text

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,8 +14,11 @@ def run_game():
     rock = Rock(settings, screen)
     paper = Paper(settings, screen)
     scissors = Scissors(settings, screen)
+    myfont = pygame.font.SysFont("monospace", 30)
     
     while True:
+        label = myfont.render("Choose either rock, paper or scissors", 1, (0,0,0))
+        screen.blit(label, (250, 100))
         # check events here
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
